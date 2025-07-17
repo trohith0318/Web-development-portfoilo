0b341cce76ce84974b8c03f46ee9e477000ed1ba
T.Rohith sri phanidhar <trsphanidhar@gmail.com>
2025-07-17 12:50:32 +0530

    Add files via upload

---
+++ portfoilo.js
@@ -0,0 +1,6 @@
+document.querySelector(".dropbtn").addEventListener("click", function(){
+  document.querySelector(".Contact-content").classList.toggle("show");
+});
+document.querySelector(".skill-dropbtn").addEventListener("click", function(){
+  document.querySelector(" .Skills-content").classList.toggle("show");
+});
